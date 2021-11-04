4a8be1ff28abbc7c69e829907981995ec75bf2a3
1nsanov <73361511+1nsanov@users.noreply.github.com>
2021-11-04 22:24:28 +0200

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        private static int id = 1;
+        static void Main(string[] args)
+        {
+            var listStudents = new List<Student>();
+            Console.WriteLine("Введите кол-во студентов.");
+            var length = int.Parse(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                listStudents.Add(SetStudent());
+            }
+            Console.WriteLine(Student.ShowCountToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Отличники:");
+            foreach (var item in listStudents)
+            {
+                if (item.Excellent())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Хорошисты:");
+            foreach (var item in listStudents)
+            {
+                if (item.Good())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Троечники:");
+            foreach (var item in listStudents)
+            {
+                if (item.Troechnik())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Список всех студентов:");
+            Output(listStudents);
+            Console.ReadLine();
+        }
+
+
+        private static void Output(IEnumerable collection)
+        {
+            if (collection != null)
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Cписок пуст.");
+            }
+        }
+        private static Student SetStudent()
+        {
+            //Console.WriteLine("Введите ФИО ученика.");
+            //var fullName = InputValidFullName();
+            var fullName = "Test " + GenerateNumber(10, 90);
+            var evaluations = new int[3];
+            for (int i = 0; i < evaluations.Length; i++)
+            {
+                evaluations[i] = GenerateNumber(3, 6);
+            }
+            return new Student(id++, fullName, evaluations);
+        }
+        private static int GenerateNumber(int min, int max)
+        {
+            return rnd.Next(min, max);
+        }
+        private static string InputValidFullName()
+        {
+            while (true)
+            {
+                string[] checkName;
+                var fullName = Console.ReadLine();
+                checkName = fullName.Split(' ');
+                if (checkName.Length == 3)
+                {
+                    return fullName;
+                }
+                else
+                {
+                    Console.WriteLine($"Введите полное имя!");
+                }
+            }
+        }
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Student
+    {
+        private int Id { get; set; }
+        private string FullName { get; }
+        private int[] Evaluations { get; } = new int[3];
+        private static int ExcellentsCount { get; set; } = 0;
+        private static int GoodsCount { get; set; } = 0;
+        private static int TroechnikiCount { get; set; } = 0;
+
+        public Student(int id, string fullName, int[] evaluations)
+        {
+            Id = id;
+            FullName = fullName;
+            Evaluations = evaluations;
+            CounterEvaluations();
+        }
+
+        public static string ShowCountToString()
+        {
+            return $"Отличники: {ExcellentsCount}\nХорошисты: {GoodsCount}\nТроечники: {TroechnikiCount}\n";
+        }
+
+        public bool Excellent()
+        {
+            var count = 0;
+            for (int i = 0; i < Evaluations.Length; i++)
+            {
+                if (Evaluations[i] == 5)
+                {
+                    count++;
+                }
+                if (count == 3)
+                {
+                    ExcellentsCount++;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Good()
+        {
+            var count = 0;
+            for (int i = 0; i < Evaluations.Length; i++)
+            {
+                if (Evaluations[i] == 3)
+                {
+                    return false;
+                }
+                else if (Evaluations[i] == 4)
+                {
+                    count++;
+                }
+                else if(Evaluations[i] == 5)
+                {
+                    count++;
+                }            
+            }
+            if (count >= 3 && count < 6)
+            {
+                GoodsCount++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Troechnik()
+        {
+            for (int i = 0; i < Evaluations.Length; i++)
+            {
+                if (Evaluations[i] == 3)
+                {
+                    TroechnikiCount++;
+                    return true;
+                }            
+            }
+            return false;
+        }
+
+        private void CounterEvaluations()
+        {
+            Excellent();
+            Good();
+            Troechnik();
+        }
+        public override string ToString()
+        {
+            return $"№{Id}| {FullName} | Оценки: {Evaluations[0]}, {Evaluations[1]}, {Evaluations[2]}";
+        }
+    }
+}
